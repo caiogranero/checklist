4257d0a3116fe8f0c76dd80f3fc538b4d2a586e0
Caio Granero <caio.granero@usp.br>
2017-06-08 23:29:32 -0300

    First commit
    I make a simple template and import a json-server api.
    
    Still needs:
    
    * Make ajax request for API to get user data;
    * Finish the template;
    * Create all features;

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <div class="ui top fixed menu">
+      <ui-grid container>
+        <ui-row>
+          <div class="item">
+            <img src="./assets/logo.png">
+          </div>
+          <div class="right menu">
+            <a class="item"><i class="large add to calendar icon"></i></a>
+            <a class="item"><i class="large alarm icon"></i></a>
+            <a class="item"><i class="large plus icon"></i></a>
+            <a class="item"><i class="large setting icon"></i></a>
+          </div>
+        </ui-row>
+      </ui-grid>
+    </div>
+     <ui-grid container>
+      <ui-row>
+        <ui-column class="four wide" id="side-bar-column">
+          <div class="ui small vertical menu" id="side-bar">
+            <a class="item">
+              <div class="ui icon input">
+                <input type="text" placeholder="Encontrar tarefa">
+                <i class="search icon"></i>
+              </div>
+            </a>
+            <a class="item">
+              <div class="ui small label">3</div>
+              <i class="large calendar outline icon"></i> Hoje
+            </a>
+            <a class="item">
+              <div class="ui small teal label">13</div>
+              <i class="large calendar icon"></i> Próximos 7 dias
+            </a>
+            <div class="item">
+              <div class="ui small label">27</div>
+              Neste mês
+            </div>
+          </div>
+        </ui-column>
+        <ui-column class="eleven wide" id="content-div">
+          <router-view></router-view>
+        </ui-column>
+      </ui-row>
+    </ui-grid>
+  </div>
+</template>
+
+<script>
+
+import { Mixin } from 'semantic-ui-vue2';
+
+export default {
+  name: 'app',
+  mixins: [Mixin],
+};
+
+</script>
+
+<style>
+
+#content-div{
+  margin-top: 40px;
+}
+
+#app {
+  /*text-align: center;*/
+  color: #2c3e50;
+  /*margin-top: 0px;*/
+}
+
+.ui.vertical.menu .item:before, .ui.menu .item:before {
+    background: #FFF;
+}
+
+#side-bar {
+  text-align: left;
+  background: #FFF;
+  margin-top: 40px;
+  height: 100%;
+  border: none;
+  box-shadow: none;
+}
+
+.ui.small.vertical.menu {
+    width: 16rem;
+    position: fixed;
+}
+
+
+i:hover, a > i:hover {
+  color: #E04F5F;
+}
+
+.ui.teal.label, .ui.teal.labels .label {
+    background-color: #E04F5F!important;
+    border-color: #E04F5F!important;
+    color: #FFF!important;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="content">
+    <ui-grid container>
+      <ui-row>
+        <ui-column class="four wide">
+          <h2>{{ period }}</h2>
+        </ui-column>
+        <ui-column class="eleven wide right aligned" id="new-task">
+          <i class="plus icon"></i> Nova tarefa
+        </ui-column>
+
+        <div class="ui fluid popup top left transition hidden">
+          <div class="ui four column divided center aligned grid">
+            <div class="column">1</div>
+            <div class="column">2</div>
+            <div class="column">3</div>
+            <div class="column">4</div>
+          </div>
+        </div>
+      </ui-row>
+    </ui-grid>
+  </div>
+</template>
+
+<script>
+
+
+import { Mixin } from 'semantic-ui-vue2';
+
+const $ = require('jquery');
+
+export default {
+  name: 'content',
+  mixins: [Mixin],
+  data() {
+    return {
+      period: 'Hoje',
+    };
+  },
+};
+
+$('.column').click(function(){
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
