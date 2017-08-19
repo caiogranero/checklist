9b8ac8a7bd0a6c7502349e3d6fbc9ecfc9570d5b
Caio Granero <caio.granero@usp.br>
2017-08-19 10:58:26 -0300

    change moment uses

--- src/components/NewTaskFast.vue
+++ src/components/NewTaskFast.vue
@@ -9,7 +9,6 @@
 			      </div>
 			      <div class="four wide field">
               <datepicker name="date" id="date" placeholder="Data" v-model="date" language="pt-br" format="dd/MM/yyyy"></datepicker>
-			        <!-- <input type="input" > -->
 			      </div>
 			      <input type="button" v-on:click="newTask()" class="ui button" value="Adicionar tarefa">
 			    </div>
@@ -20,56 +19,53 @@
 </template>
 
 <script>
+import { Mixin } from 'semantic-ui-vue2';
+import Tasks from './Tasks';
+import Datepicker from 'vuejs-datepicker';
+import mixins from './mixins';
 
-  import { Mixin } from 'semantic-ui-vue2';
-  import Tasks from './Tasks';
-  import Datepicker from 'vuejs-datepicker';
-
-  const moment = require('moment');
-
-  export default {
-    name: 'NewTaskFast',
-    components: {
-      Tasks,
-      Datepicker
-    },
-    mixins: [Mixin],
-    data() {
-      return {
-        date: null,
-        name: null
-      };
-    },
-    methods: {
-
-      // Insert a new task in DB, based in name and date values inserted.
-      newTask: function() {
-
-        moment.locale('pt-br');
-
-        let data = {
-          name: this.name,
-          task_date: moment(this.date, "DD/MM/YYYY").startOf('day').format(),
-          task_week: moment(this.date, "DD/MM/YYYY").week(),
-          task_month: moment(this.date, "DD/MM/YYYY").month(),
-          last_update: moment().format(),
-          created_at: moment().format(),
-          userId: 1,
-          isRemoved: false,
-          isOpen: true,
-          isCompleted:false
-        };
-
-        this.$http.post('http://localhost:3000/users/1/tasks', JSON.stringify(data)).then(function(response) {
-          this.$parent.$parent.loadCurrentTasks();
-          this.name = null;
-          this.date = null;
-
-          this.$swal("Tarefa criada com sucesso!", "", "success");
-        });
-      }
+export default {
+  name: 'NewTaskFast',
+  components: {
+    Tasks,
+    Datepicker
+  },
+  mixins: [mixins, Mixin],
+  data() {
+    return {
+      date: null,
+      name: null
     }
-  };
+  },
+  methods: {
+
+    // Insert a new task in DB, based in name and date values inserted.
+    // TODO: Need to check if all fields all filled before insert in database.
+    newTask() {
+
+      let data = {
+        name: this.name,
+        task_date: this.moment(this.date, "DD/MM/YYYY").startOf('day').format(),
+        task_week: this.moment(this.date, "DD/MM/YYYY").week(),
+        task_month: this.moment(this.date, "DD/MM/YYYY").month(),
+        last_update: this.moment().format(),
+        created_at: this.moment().format(),
+        userId: 1,
+        isRemoved: false,
+        isOpen: true,
+        isCompleted:false
+      }
+
+      this.$http.post('http://localhost:3000/users/1/tasks', JSON.stringify(data)).then((response) => {
+        this.$parent.$parent.loadCurrentTasks()
+        this.name = null
+        this.date = null
+
+        this.$swal("Tarefa criada com sucesso!", "", "success")
+      })
+    }
+  }
+}
 
 
 </script>
