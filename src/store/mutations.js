f98372a60e68ec9c966e6cf9d52a5630f2b5c36d
Caio Granero <caio.granero@usp.br>
2017-08-19 10:57:57 -0300

    start to use Vuex

---
+++ src/store/mutations.js
@@ -0,0 +1,8 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
+const mutations = {
+  setFilter(state, filter) {
+    state.filter = filter;
+  },
+};
+
+export default mutations;
---
+++ src/store/state.js
@@ -0,0 +1,5 @@
+const state = {
+  filter: '',
+};
+
+export default state;
